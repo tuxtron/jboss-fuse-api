575fe189b281d42878e0605f37e1fa98cf90082f
tuxtron <32469400+tuxtron@users.noreply.github.com>
2020-03-02 00:28:41 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,10 +5,12 @@ sleep 60 && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin features:install camel-quartz2 && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin features:install camel-script-javascript && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin features:install camel-velocity/2.15.1.redhat-621084 && \
-/opt/jboss/jboss-full/bin/client -u admin -p admin features:install camel-jackson/2.15.1.redhat-621084 && \
+/opt/jboss/jboss-full/bin/client -u admin -p admin features:install camel-xmljson/2.15.1.redhat-621084 && \
+/opt/jboss/jboss-full/bin/client -u admin -p admin features:install camel-http4/2.15.1.redhat-621084 && \
+/opt/jboss/jboss-full/bin/client -u admin -p admin features:install camel-jsonpath/2.15.1.redhat-621084 && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin features:install cxf-rs-security-cors/3.0.4.redhat-621084 && \
-/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s wrap:mvn:com.microsoft.sqlserver/sqljdbc4/4.0' && \
-/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s wrap:mvn:com.sybase/jconn3/6.0.26312' && \
+/opt/jboss/jboss-full/bin/client -u admin -p admin features:install spring-web/3.2.12.RELEASE_2 && \
+/opt/jboss/jboss-full/bin/client -u admin -p admin features:install spring-jdbc/3.2.12.RELEASE_2 && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:com.atomikos/transactions-api/3.9.3' && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:com.atomikos/transactions-jdbc/3.9.1' && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:com.atomikos/transactions-jta/3.9.1' && \
@@ -26,18 +28,11 @@ sleep 60 && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.threeten/threetenbp/1.3.3' && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:com.doctusoft/json-schema-java7/1.4.1.1' && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:commons-beanutils/commons-beanutils/1.9.4' && \
-#/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.codehaus.jackson/jackson-core-asl/1.9.0' && \
+/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.codehaus.jackson/jackson-core-asl/1.9.0' && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.codehaus.jackson/jackson-mapper-asl/1.9.0' && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.codehaus.jackson/jackson-jaxrs/1.9.0' && \
-/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:bundle-level --force fuse-bundle-ds-sybase-prestaci 50' && \
-/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:bundle-level --force fuse-bundle-ds-sybase-gps-smg 50' && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.apache.httpcomponents/httpcore-osgi/4.3.3' && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.apache.httpcomponents/httpclient-osgi/4.3.6' && \
-/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.springframework/spring-core/3.0.2.RELEASE' && \
-/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.springframework/spring-aop/3.0.2.RELEASE' && \
-/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.springframework/spring-beans/3.0.2.RELEASE' && \
-/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.springframework/spring-context/3.0.2.RELEASE' && \
-/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.springframework/spring-web/3.0.2.RELEASE' && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.apache.camel/camel-http4/2.15.0' && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.apache.camel/camel-core/2.15.0' && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:net.minidev/asm/1.0.2' && \
@@ -45,4 +40,6 @@ sleep 60 && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:com.jayway.jsonpath/json-path/1.2.0' && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'shutdown -f' && \
 sleep 10
+
+
 
